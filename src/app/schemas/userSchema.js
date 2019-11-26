0fb7b8ad76e1a2f296c471c84f180810de62b59f
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2019-11-26 01:04:43 -0300

    finish

--- src/app/schemas/userSchema.js
+++ src/app/schemas/userSchema.js
@@ -48,8 +48,6 @@ const userSchema = new moongose.Schema({
 });
 
 userSchema.pre('save', async function(next) {
-  // if (!this.isModified('password')) return next();
-
   this.password = await bcrypt.hash(this.password, 12);
 
   this.passwordConfirm = undefined;
