4e87394ed5ff5b66c11baf9037e3ef8b2690b580
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2022-03-24 10:01:18 -0300

    test1: update

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+  console.log(`App running on port :DS ${port}`);
 });
